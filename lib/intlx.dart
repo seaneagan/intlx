b3d822109cc679dd86b852b5e94d3edc33583245
Sean Eagan <seaneagan1@gmail.com>
2013-05-02 10:53:17 -0500

    move TimeUnit to its own file

--- lib/intlx.dart
+++ lib/intlx.dart
@@ -9,6 +9,7 @@ import 'src/iterable/iterable_locale.dart';
 import 'src/relative_time/duration_converters.dart';
 
 part 'src/relative_time/relative_time_format.dart';
+part 'src/relative_time/time_unit.dart';
 part 'src/plural/plural_format.dart';
 part 'src/iterable/iterable_format.dart';
 part 'src/format_length.dart';
---
+++ lib/src/relative_time/time_unit.dart
@@ -0,0 +1,28 @@
+
+part of intlx;
+
+/// Enum to represent time units
+class TimeUnit implements Comparable<TimeUnit> {
+  // TODO: this class should be in a generic calendar library, 
+  // such as that referenced in http://dartbug.com/5627
+  
+  static const SECOND = const TimeUnit._("SECOND", 0);
+  static const MINUTE = const TimeUnit._("MINUTE", 1);
+  static const HOUR = const TimeUnit._("HOUR", 2);
+  static const DAY = const TimeUnit._("DAY", 3);
+  static const WEEK = const TimeUnit._("WEEK", 4);
+  static const MONTH = const TimeUnit._("MONTH", 5);
+  static const YEAR = const TimeUnit._("YEAR", 6);
+
+  const TimeUnit._(this._name, this._index);
+
+  int compareTo(TimeUnit other) => _index.compareTo(other._index);
+
+  String toString() => _name;
+
+  final String _name;
+  final int _index;
+
+  static const values = const <TimeUnit> [SECOND, MINUTE, HOUR, DAY, WEEK, MONTH, YEAR];
+}
+
